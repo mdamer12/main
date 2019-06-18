b9e4b192e7c3134c8f771901804be017aaedc51a
mdamer12 <51962114+mdamer12@users.noreply.github.com>
2019-06-18 17:48:42 +0300

    Add files via upload

---
+++ New Text Document.js
@@ -0,0 +1,15 @@
+{
+  "version": "v2",
+  "content": {
+    "messages": [
+      {
+        "type": "video",
+        "url": "https://www.facebook.com/facebook/videos/2397728427125712",
+        "buttons": []
+      }
+    ],
+    "actions": [],
+    "quick_replies": []
+  }
+}
+
